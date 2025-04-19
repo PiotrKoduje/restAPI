90860e220ed77a235e6c300df9dd032ccc725457
Piotr Pelc <piotrpelc83@gmail.com>
2025-04-19 17:42:25 +0200

    Add middlewares

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(cors());
+
 app.listen(6000, () => {
   console.log('Server is running on port: 6000');
 });
